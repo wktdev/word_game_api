81dd18acf58182a5e02047b0e38503d85276ec37
wktdev <wktdev@gmail.com>
2016-10-26 12:01:05 -0700

    initial

---
+++ app.js
@@ -0,0 +1,172 @@
+'use strict';
+
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
+var jwt = require("jwt-simple");
+var path = require('path');
+const Sequelize = require("sequelize");
+const connection = new Sequelize("wordapp", "root", "password");
+const bcrypt = require("bcryptjs");
+const salt = bcrypt.genSaltSync(10);
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+var secret = 'aliens';
+
+//______________________________________________________________BEGIN user model
+
+const User = connection.define("user", {
+    username: {
+        type: Sequelize.STRING
+    },
+
+    email: {
+        type: Sequelize.STRING,
+        unique: true
+
+    },
+    password: {
+        type: Sequelize.TEXT
+    }
+
+});
+
+connection.sync();
+
+//______________________________________________________________END user model
+
+
+
+//______________________________________________________________BEGIN enable CORS
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+//_____________________________________________________________END enable CORS
+
+
+
+
+app.post("/login", function(req, res) {
+
+    var userEmail = req.body.email;
+    var enteredPassword = req.body.password;
+
+
+    User.findOne({
+        where: {
+            email: userEmail
+        }
+    }).then(function(user) {
+        if (user) {
+            var correctPassword = bcrypt.compareSync(enteredPassword, user.password);
+
+            if (correctPassword) {
+                console.log("User exists")
+            } else {
+                console.log("Email or password is invalid")
+            }
+        } else {
+            console.log("That email is invalid")
+        }
+    });
+
+});
+
+
+//______________________________________________________________BEGIN save registration data to database
+
+
+app.post("/register", function(req, res) {
+    //__________________________________________________________setup variables
+
+    var passwordEncrypted = bcrypt.hashSync(req.body.password, salt),
+        payload = { username: req.body.username, email: req.body.email, password: passwordEncrypted },
+        token = jwt.encode(payload, secret),
+        decoded = jwt.decode(token, secret);
+    //__________________________________________________________END setup variables
+
+    User.findOne({
+        where: {
+            email: payload.email
+        },
+    }).then(function(user) {
+
+        if (user) {
+
+            console.log("-------EMAIL IS IN USE -----");
+            res.status(409).json('internal server error');
+
+
+        } else {
+
+            var user = User.build({
+                username: payload.username,
+                email: payload.email,
+                password: payload.password
+            });
+
+            user.save()
+
+            res.json(token);
+
+        }
+    });
+
+
+});
+
+//_____________________________________________________________END save registration data to database
+
+
+//_____________________________________________________________BEGIN send api data
+app.post("/api/level", function(req, res) {
+
+    var decoded_JWT = jwt.decode(req.body.jwt, secret);
+    var userEmail = decoded_JWT.email;
+    var userPassword = decoded_JWT.password;
+
+    console.log(decoded_JWT)
+
+    var levelData = {
+        level: "Some level data and stuff"
+    }
+
+    User.findOne({
+        where: {
+            email: userEmail,
+            password: userPassword
+
+        },
+    }).then(function(user) {
+        if (user) {
+
+            res.json(levelData);
+
+        } else {
+            res.status(409)
+        }
+    })
+
+
+
+
+});
+
+app.get("*", function(req, res) {
+        res.redirect("/")
+    })
+    //________________________________________________________________END send api data
+
+app.listen(3000);
+
+console.log("listening on 3000")
---
+++ public/js/app.js
@@ -0,0 +1,74 @@
+$(function(){
+
+var website = "http://localhost:3000";
+
+$('.user-login').on('submit',function(e){
+
+		e.preventDefault();
+		var $email = $("input[name='login-email']").val();
+		var $password = $("input[name='login-password']").val();
+
+		$.post(website+"/login",{email:$email, password:$password},function(data){
+	
+			if(data !== 404){
+
+				localStorage.setItem("wordmill_jwt", data );
+				$(".login-validation").text("logged in");
+
+			}else{
+
+				console.log(data)
+			}
+
+		});		
+});
+
+
+//___________________________________________________BEGIN get api level data
+	$(".get-level-data").on("submit",function(e){
+		e.preventDefault(); 
+		var jwtFromBrowser = localStorage.getItem('wordmill_jwt');
+	
+		$.post(website + "/api/level",{jwt:jwtFromBrowser},function(data){
+
+            console.log(data);
+
+		});
+	});
+//____________________________________________________END get api level data
+
+
+//_________________________________________________BEGIN register
+ 
+	$('.user-register').on('submit',function(e){
+		e.preventDefault();
+
+		var $username = $("input[name='username']").val();
+		var $email = $("input[name='email']").val();
+		var $password = $("input[name='password']").val();
+var user = {username:$username, email:$email, password:$password};
+console.log(user)
+
+		$.post(website+"/register",{username:$username, email:$email, password:$password},function(data){
+			localStorage.removeItem("wordmill_jwt");
+			
+
+			
+			if(data !== 404){
+
+				localStorage.setItem("wordmill_jwt", data );
+				$(".login-validation").text("logged in");
+
+			}else{
+
+				console.log(data)
+			}
+			
+		});
+
+	});
+//___________________________________________________END register
+
+
+
+});
